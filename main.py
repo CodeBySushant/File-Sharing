4a332e9433081c2a11d40c94d6fb1ede7de4cbb0
Sushant Sharma <itsofficialsushant@gmail.com>
2026-05-17 05:04:48 +0545

    Minor Change -> Proper Data Handling in SQLite

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import os
 app = FastAPI()
 
 DATABASE_URL = "sqlite:///./test.db"
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base.metadata.create_all(bind=engine)
 
